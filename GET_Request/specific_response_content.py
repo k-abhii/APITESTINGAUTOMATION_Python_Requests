e43754b1ed719d5a2618613c02fb5b7abc9b41e1
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-10 20:49:47 +0530

    JSONPATH

--- GET_Request/specific_response_content.py
+++ GET_Request/specific_response_content.py
@@ -1,3 +1,4 @@
+import jsonpath
 import  requests
 import json
 url = "https://reqres.in/api/users?page=2"
@@ -8,7 +9,7 @@ response = requests.get(url)
 
 # Parse response to Json Format
 response_json = json.loads(response.text)
-print(response_json)
+# print(response_json)  
 # {'page': 2, 'per_page': 6, 'total': 12, 'total_pages': 2,
 # 'data': [{'id': 7, 'email': 'michael.lawson@reqres.in', 'first_name': 'Michael',
 # 'last_name': 'Lawson', 'avatar': 'https://reqres.in/img/faces/7-image.jpg'},
@@ -21,3 +22,7 @@ print(response_json)
 # {'id': 12, 'email': 'rachel.howell@reqres.in', 'first_name': 'Rachel', 'last_name': 'Howell',
 # 'avatar': 'https://reqres.in/img/faces/12-image.jpg'}], 'support': {'url': 'https://reqres.in/#support-heading',
 # text': 'To keep ReqRes free, contributions towards server costs are appreciated!'}}
+
+# Fetch Value Using Jsonpath --> It will return list
+pages = jsonpath.jsonpath(response_json,'total_pages')
+print(pages[0])
