8214b7deebeb16f5927f739073b4199039ef01e6
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-10 17:50:13 +0530

    Get Started With Rest API

---
+++ GET_Request/FetchUserData.py
@@ -0,0 +1,11 @@
+import requests
+
+# API URL
+url = "https://reqres.in/api/users?page=2"
+
+#Send Get Request
+response = requests.get(url)
+print(response)
+
+# Display Response Content
+print(response.content)
