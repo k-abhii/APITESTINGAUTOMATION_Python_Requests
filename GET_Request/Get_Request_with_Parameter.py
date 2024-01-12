b67281a4e3e5bac7cb6499c5171125eca0b28da9
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-12 16:50:39 +0530

    Get Request with Parameter

---
+++ GET_Request/Get_Request_with_Parameter.py
@@ -0,0 +1,4 @@
+import requests
+param = {'name':'testingworld','email':'testingworldindia@gmail.com','number':'+91-8743-913121'}
+response = requests.get("https://httpbin.org/get",params=param)
+print(response.text)
